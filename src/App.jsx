df8905956bc8c9ac17af5c00243c5dab97fd1129
Sinethemba <sinethemba@wealthmigrate.com>
2022-08-14 00:30:22 +0200

    created boilerplate for the application

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes
+} from 'react-router-dom';
+import './App.css';
+import  Signin from './views/signin';
+
+function App () {
+
+  return (
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Navigate to="/signin" replace />}></Route>
+          <Route
+          exact
+          path="/signin"
+          element={<Signin />}
+          >
+          </Route>
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
+
